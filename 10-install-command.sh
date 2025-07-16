bc490c1efe34850c7db80c956a4f7a49c1677a7b
kalyankumar <kalyank608@gmail.com>
2025-07-16 11:32:42 +0530

    special vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -8,14 +8,31 @@ then
     exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
-fi        
+fi
 
-dnf install mysql -y
+dnf list installed mysql
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    echo "Installing mysql is ... SUCCESS"
+    echo "MYSQL is not installed ... going to install it"
+    dnf install mysql -y
+
+    if [ $? -eq 0 ]
+    then
+        echo "Installing mysql is ... SUCCESS"
+    else
+        echo "Installing mysql is ... FAILURE"
+        exit 1
+    fi             
 else
-    echo "Installing mysql is ... FAILURE"
-    exit 1
-fi         
+    echo "MYSQL is already installed ...nothing to do"
+fi        
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing mysql is ... SUCCESS"
+# else
+#     echo "Installing mysql is ... FAILURE"
+#     exit 1
+# fi         
