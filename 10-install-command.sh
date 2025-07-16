455f2d23702d2919e876d25b946e244af2ed3bc4
kalyankumar <kalyank608@gmail.com>
2025-07-16 11:09:06 +0530

    special vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -14,7 +14,7 @@ dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
-    echo "Installing mysqlgdsg is ... SUCCESS"
+    echo "Installing mysqllfjdfdaf is ... SUCCESS"
 else
     echo "Installing mysql is ... FAILURE"
     exit 1
