834c1ff86e78b48667c23adf2d03e51eb17f1b7a
kalyankumar <kalyank608@gmail.com>
2025-07-24 10:04:23 +0530

    hello world started

--- 20-backup.sh
+++ 20-backup.sh
@@ -57,3 +57,12 @@ then
     echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+if [ ! -z $FILES ]
+then
+    echo "Files found"
+else
+    echo "No log files found older than 14 days ... $Y SKIPPING $N"
+fi    
